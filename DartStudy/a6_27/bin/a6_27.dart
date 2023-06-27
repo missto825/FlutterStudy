e747876adb2f74966f3361885665e02e500f32fc
missto825 <suhwan825@gmail.com>
2023-06-27 12:37:18 +0900

    NewDart

---
+++ DartStudy/a6_27/bin/a6_27.dart
@@ -0,0 +1,5 @@
+import 'package:a6_27/a6_27.dart' as a6_27;
+
+void main(List<String> arguments) {
+  print(10);
+}
---
+++ DartStudy/a6_27/test/a6_27_test.dart
@@ -0,0 +1,8 @@
+import 'package:a6_27/a6_27.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
